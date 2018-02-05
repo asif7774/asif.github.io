54dc2725b688aa0046b79a8a6440f6c359740d01
asif ansari <asif.ansari7774@gmail.com>
2018-02-05 14:49:43 +0530

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -9,32 +9,32 @@
   "theme_color": "#d2d2d2",
   "icons": [
       {
-        "src": "img/aha_128.png",
+        "src": "/img/aha_128.png",
         "type": "image/png",
         "sizes": "128x128"
         },
         {
-        "src": "img/aha_152.png",
+        "src": "/img/aha_152.png",
         "type": "image/png",
         "sizes": "152x152"
         },
         {
-        "src": "img/aha_144.png",
+        "src": "/img/aha_144.png",
         "type": "image/png",
         "sizes": "144x144"
         },
         {
-        "src": "img/aha_192.png",
+        "src": "/img/aha_192.png",
         "type": "image/png",
         "sizes": "192x192"
         },
         {
-        "src": "img/aha_256.png",
+        "src": "/img/aha_256.png",
         "type": "image/png",
         "sizes": "256x256"
         },
         {
-        "src": "img/aha_512.png",
+        "src": "/img/aha_512.png",
         "type": "image/png",
         "sizes": "512x512"
         }
